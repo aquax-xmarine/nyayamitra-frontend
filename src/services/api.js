e330e058e0fcb0afa54b33e909483b9155007080
Nikisha <snikisha002@gmail.com>
2026-05-30 01:36:30 +0545

    add ngrok header

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,8 @@ const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true'
   }
 });
 
